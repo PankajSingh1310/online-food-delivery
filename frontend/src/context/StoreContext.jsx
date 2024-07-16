a00ef6671337b855b6cfb125ac0d9236749fa856
PankajSingh1310 <130424788+PankajSingh1310@users.noreply.github.com>
2024-07-16 12:26:16 +0530

    Backend url update

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-  const url = "http://localhost:4000";
+  const url = "https://online-food-delivery-backend.onrender.com";
 
   const [cartItems, setCartItems] = useState({});
   const [token, setToken] = useState("");
